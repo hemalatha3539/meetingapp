d16383cb87356531fb102d60bd01ad2ab95e9d98
hemalatha3539 <139043342+hemalatha3539@users.noreply.github.com>
2023-12-19 10:36:54 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -3,13 +3,13 @@ const Nav = () => {
     return (
       <div className="navbar">
         <div className="t-flex">
-            <div className="title">
-               <h1>LetsMeet</h1>  
+           <div className="title">
+               <h1><i class="bi bi-person-workspace"></i>  LetsMeet</h1>  
             </div>
             <ul className="nav-items">
-                <Link to="/" className="homee"><li>All Meets</li></Link>
-                <Link to="/Mymeet" className="homee"><li>My Meet</li></Link>
-               <Link to="/Addmeet" className="homee"> <li>Add Meet</li> </Link>
+                <Link to="/" className="homee"><li><i class="bi bi-people"></i> All Meets</li></Link>
+                <Link to="/Mymeet" className="homee"><li><i class="bi bi-person-fill-gear"></i> My Meet</li></Link>
+               <Link to="/Addmeet" className="homee"> <li><i class="bi bi-person-fill-add"></i> Add Meet</li> </Link>
                 
                  
             </ul>
